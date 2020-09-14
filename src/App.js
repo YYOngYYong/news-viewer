81bd1ce758035dced076733f84d031deb2565a7d
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-09-14 14:24:55 +0900

    json stringfy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "http://newsapi.org/v2/top-headlines?country=kr&apiKey=3706736d52da4f97b8505197e1ae8f74"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={2}
+          value={JSON.stringify(data, null, 3)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
