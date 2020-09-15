6789da738bf618037930a1a4b1c9c41fa3b203f1
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-09-15 10:51:16 +0900

    category styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 const App = () => {
-  return <NewsList />;
+  return (
+    <>
+      <Categories />
+      <NewsList />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+const categories = [
+  {
+    name: "all",
+    text: "전체보기",
+  },
+  {
+    name: "business",
+    text: "비즈니스",
+  },
+  {
+    name: "entertainment",
+    text: "엔터테인먼트",
+  },
+  {
+    name: "health",
+    text: "건강",
+  },
+  {
+    name: "science",
+    text: "과학",
+  },
+  {
+    name: "sports",
+    text: "스포츠",
+  },
+  {
+    name: "technology",
+    text: "기술",
+  },
+];
+
+const CategoriesBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
+`;
+
+const Category = styled.div`
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 1.12rem;
+  transition: all 300ms ease;
+
+  &:hover {
+    color: blue;
+    transform: scale(1.1);
+  }
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+const Categories = () => {
+  return (
+    <CategoriesBlock>
+      {categories.map((c) => (
+        <Category key={c.name}>{c.text}</Category>
+      ))}
+    </CategoriesBlock>
+  );
+};
+export default Categories;
