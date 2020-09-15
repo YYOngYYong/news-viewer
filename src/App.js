ba22906c8d98c3c3c601aca4f539f8f2c97b4914
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-09-15 10:16:34 +0900

    newsList

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "http://newsapi.org/v2/top-headlines?country=kr&apiKey=3706736d52da4f97b8505197e1ae8f74"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={2}
-          value={JSON.stringify(data, null, 3)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
---
+++ src/components/NewsList.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import NewsItem from "./NewsItem";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  const [article, setArticle] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // async 사용하는 내부 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://newsapi.org/v2/top-headlines?country=kr&apiKey=3706736d52da4f97b8505197e1ae8f74"
+        );
+        setArticle(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // 대기중
+  if (loading) {
+    return <NewsListBlock>로딩중.....</NewsListBlock>;
+  }
+  if (!article) {
+    return null;
+  }
+
+  //article 값이 있을때만
+  return (
+    <NewsListBlock>
+      {article.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
