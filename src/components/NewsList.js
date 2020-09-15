5cd050a63d2198d0ad1d1f5d12183ea0b98c0784
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-09-15 11:19:30 +0900

    function : category

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [article, setArticle] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -25,8 +25,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === "all" ? "" : `&category=${category}`;
         const response = await axios.get(
-          "http://newsapi.org/v2/top-headlines?country=kr&apiKey=3706736d52da4f97b8505197e1ae8f74"
+          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=3706736d52da4f97b8505197e1ae8f74`
         );
         setArticle(response.data.articles);
       } catch (e) {
@@ -35,7 +36,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   // 대기중
   if (loading) {
